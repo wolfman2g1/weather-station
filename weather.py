72c50d2ac6a13affc43c25b8c859f3bcd3454993
Ryan Jones <ryan.jones26@gmail.com>
2020-05-04 08:33:58 -0400

    Need to fix the time precision issue when writing to influx

--- weather.py
+++ weather.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import serial
 import time
+import calendar
 from influxdb import InfluxDBClient
 import configparser
+import json
 
 ## check that config exists
 try:
@@ -16,19 +18,20 @@ serial_rate = parser.get('Serial', 'rate')
 server = parser.get('Database', 'server')
 db = parser.get('Database', 'db')
 db_port = parser.get('Database', 'db_port')
-
-
+time = calendar.timegm(time.gmtime())
+#timestamp = time.strftime('%Y-%m-%dT%H:%M:%S')
 
 
 def create_db():
     client = InfluxDBClient(host=server, port=db_port)
-    client.create_database('weather')
-    #start()
+    print('Create Database: ' + db)
+    client.create_database(db)
+    # start()
 
 
 def store_data(json_data):
     client = InfluxDBClient(host=server, port=db_port)
-    client.write_points('weather', json_data)
+    client.write_points('weather', json_data, time_precision='s')
 
 
 def mak_num(thing):
@@ -52,22 +55,23 @@ def mak_float(thing):
 
 def start():
     create_db()
-    ser = serial.Serial(serial_port, serial_rate, timeout=2)
+    ser = serial.Serial(serial_port, serial_rate, timeout=2, )
     while True:
         data = ser.readlines()
         if len(data) > 0:
             goodData = data[0].decode("utf-8").rstrip().split(',')
-            print(goodData)
-            json_data = {"timestamp": time.time(),
+            list_data = {"timestamp": time,
                          "wind_Speed": mak_num(goodData[0]),
-                         "wind_dir": goodData[1],
+                         "wind_dir": mak_num(goodData[1]),
                          "temp_c": mak_num(goodData[2]),
                          "temp_f": mak_num(goodData[3].split(".")[0]),
                          "humidity": mak_num(goodData[4]),
                          "baro": mak_num(goodData[5].split(".")[0]),
                          "rain": mak_num(goodData[6])}
-            
-            #print(json_data)
+            json_data = json.dumps(list_data, indent=2)
+            store_data(json_data)
+
+            #print(list_data)
     ser.close()
 
 
