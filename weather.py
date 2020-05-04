54b2bd98d56f5556394b7a270b50ed0294a6b6d8
Ryan Jones <ryan.jones26@gmail.com>
2020-05-04 10:14:46 -0400

    progress

--- weather.py
+++ weather.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 import serial
 import time
-import calendar
 from influxdb import InfluxDBClient
+from influxdb import exceptions
 import configparser
 import json
 
-## check that config exists
+# check that config exists
 try:
     parser = configparser.ConfigParser()
     parser.read('config.ini')
 except configparser.ParsingError as err:
     print("Config not Found", err)
-## import config data from file
+# import config data from file
 serial_port = parser.get('Serial', 'port')
 serial_rate = parser.get('Serial', 'rate')
 server = parser.get('Database', 'server')
 db = parser.get('Database', 'db')
 db_port = parser.get('Database', 'db_port')
-time = calendar.timegm(time.gmtime())
-#timestamp = time.strftime('%Y-%m-%dT%H:%M:%S')
+
+
+# timestamp = time.strftime('%Y-%m-%dT%H:%M:%S')
 
 
 def create_db():
@@ -29,9 +30,31 @@ def create_db():
     # start()
 
 
-def store_data(json_data):
+def set_up(json_data):
+    measurement = 'weather_data'
+    number_of_points = 10
+    timestamp = int(time.time() * 1000)
+    json_data = json_data
+    data = []
+    data.append(
+        {
+            "measurement": measurement,
+            "timestamp": timestamp,
+            "fields": {
+                "data": json_data
+            }
+        }
+    )
+    store_data(data)
+
+
+def store_data(data):
     client = InfluxDBClient(host=server, port=db_port)
-    client.write_points('weather', json_data, time_precision='s')
+    try:
+        client.write_points(data, database=db,
+                            protocol='line', time_precision='ms')
+    except exceptions.InfluxDBClientError as err:
+        print("Uh oh", err)
 
 
 def mak_num(thing):
@@ -51,6 +74,13 @@ def mak_float(thing):
     except:
         thing2 = 0
     return thing2
+def make_empty(thing):
+    try:
+        if "??" in thing:
+            thing = " "
+    except:
+        thing = "null"
+    return thing
 
 
 def start():
@@ -59,19 +89,26 @@ def start():
     while True:
         data = ser.readlines()
         if len(data) > 0:
+            measurement_name = 'weather_data'
             goodData = data[0].decode("utf-8").rstrip().split(',')
-            list_data = {"timestamp": time,
-                         "wind_Speed": mak_num(goodData[0]),
-                         "wind_dir": mak_num(goodData[1]),
-                         "temp_c": mak_num(goodData[2]),
-                         "temp_f": mak_num(goodData[3].split(".")[0]),
-                         "humidity": mak_num(goodData[4]),
-                         "baro": mak_num(goodData[5].split(".")[0]),
-                         "rain": mak_num(goodData[6])}
-            json_data = json.dumps(list_data, indent=2)
-            store_data(json_data)
+            timedata = int(time.time() * 1000)
+            wind_Speed = mak_num(goodData[0])
+            wind_dir = make_empty(goodData[1])
+            temp_c = mak_num(goodData[2])
+            temp_f = mak_num(goodData[3].split(".")[0])
+            humidity = mak_num(goodData[4])
+            baro = mak_num(goodData[5].split(".")[0])
+            rain = mak_num(goodData[6])
+            data = []
+            data.append(
+                "{measurement},wind_direction={wind_dir},wind_velocity={wind_speed},fahrenheit={tempF}, celcius={tempC}, humidit={relative_hum}, preasure={qnh}, rain={precip}, time={timeinfo}".format(
+                    measurement=measurement_name, wind_dir=wind_dir, wind_speed=wind_Speed, tempF=temp_f, tempC=temp_c, relative_hum=humidity, qnh=baro, precip=rain, timeinfo=timedata
+                ))
+            #json_data = json.dumps(list_data, indent=2)
+            store_data(data)
 
-            #print(list_data)
+
+            # print(list_data)
     ser.close()
 
 
